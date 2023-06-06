f5ebab282d8767f143d159b53c9891499cf30d84
Gabriely Carrari <gabymcarrari@gmail.com>
2023-06-06 10:43:45 -0300

    add spring

---
+++ spring/src/test/java/com/gabriely/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriely.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
